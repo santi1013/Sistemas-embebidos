ba58c2cd7e5e15180c4500701cf154f0cf8109f9
santi1013 <santimartinezr@unicauca.edu.co>
2026-03-03 16:53:38 -0500

    Update Sensors.h

--- maquina/Sensors.h
+++ maquina/Sensors.h
@@ -7,7 +7,7 @@
 // Variables compartidas de sensores
 extern char entrada;
 extern bool ledState;
-
+extern bool boton;
 // Funciones de los diferentes sensores y procesos complementarios
 void leerEntrada();
 void toggleLed1();
@@ -18,7 +18,11 @@ void promLuz(void);
 void enviarLuz(void);
 //funcion del sensor de flama 
 void leerFlama(void);
-
+void rgbRed();
+void rgbGreen();
+void rgbBlue();
+void readButton();
 
 #endif
+
 
