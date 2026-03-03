fd985a6b489157f6351a855205cbb74f4118e1c3
santi1013 <santimartinezr@unicauca.edu.co>
2026-03-03 17:17:41 -0500

    Update Tasks.cpp

--- maquina/Tasks.cpp
+++ maquina/Tasks.cpp
@@ -7,9 +7,10 @@ AsyncTask taskLeer(1800, true, []() { leerLuz(); });
 AsyncTask taskEnviar(1, false, []() { enviarLuz(); });
 
 //del sensor flama 
-AsyncTask taskLeer(periodoLectura, true, []() { leerFlama(); });
+AsyncTask taskLeerFlama(periodoLectura, true, []() { leerFlama(); });
 
 AsyncTask taskGreen  (200, true,  []() { rgbGreen();    });
 AsyncTask taskBoton  (10, true,   []() { readButton();  });
+
 
 
