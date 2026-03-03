7198665988b16c7204cd491c248ea94a74f586d9
Viviana A. Ortega <vaortega@unicauca.edu.co>
2026-03-03 16:46:16 -0500

    Update Tasks.h

--- maquina/Tasks.h
+++ maquina/Tasks.h
@@ -6,5 +6,13 @@
 
 extern AsyncTask taskEntrada;
 
+//tareas del sensor de luz
+extern AsyncTask taskLeer;
+extern AsyncTask taskPromediar;
+extern AsyncTask taskEnviar;
+//tareas del sensor de flama
+extern AsyncTask taskLeer;
+
+
 
 #endif
