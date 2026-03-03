80c6ee679072674fc0b457c1c408912d06d26edd
Viviana A. Ortega <vaortega@unicauca.edu.co>
2026-03-03 16:48:10 -0500

    Update Tasks.cpp

--- maquina/Tasks.cpp
+++ maquina/Tasks.cpp
@@ -1,3 +1,12 @@
 #include "Tasks.h"
 
 AsyncTask taskEntrada(20,  true,  []() { leerEntrada(); });
+
+//del sensor de luz
+AsyncTask taskLeer(1800, true, []() { leerLuz(); });
+AsyncTask taskPromediar(1, false, []() { promLuz(); });
+AsyncTask taskEnviar(1, false, []() { enviarLuz(); });
+
+//del sensor flama 
+AsyncTask taskLeer(periodoLectura, true, []() { leerFlama(); });
+
